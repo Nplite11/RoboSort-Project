46a62c9d61ec743f59c1256867d6fb035b659d7e
Nplite <76342ns@gmail.com>
2023-12-06 10:45:38 +0530

    logger file updated

---
+++ RoboSort Implimentation/logger/__init__.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+import logging
+import os
+from datetime import datetime
+
+LOG_DIR= "Robosort_logs"
+
+CURRENT_TIME_STEMP= f"{datetime.now().strftime('%Y-%m-%d %H-%M-%S')}"
+
+log_file_name= f"log{CURRENT_TIME_STEMP}.log"
+
+os.makedirs(LOG_DIR, exist_ok=True)
+
+log_file_path= os.path.join(LOG_DIR,log_file_name)
+
+logging.basicConfig(filename=log_file_path,
+filemode="w",
+format='[%(asctime)s]^;%(levelname)s^;%(lineno)d^;%(filename)s^;%(funcName)s()^;%(message)s',
+level=logging.INFO
+)
+
+def get_log_dataframe(file_path):
+    data=[]
+    with open(file_path) as log_file:
+        for line in log_file.readlines():
+            data.append(line.split("^;"))
+
+    log_df = pd.DataFrame(data)
+    columns=["Time stamp","Log Level","line number","file name","function name","message"]
+    log_df.columns=columns
+    
+    log_df["log_message"] = log_df['Time stamp'].astype(str) +":$"+ log_df["message"]
+
+    return log_df[["log_message"]]
